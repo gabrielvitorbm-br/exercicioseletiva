9d7c3efda2b7cf3bf749dfcee9f1eb9ea86adc61
Gabriel <gabrielvitorbm@gmail.com>
2026-08-12 20:39:40 -0300

    primeiro

---
+++ resources/views/exer1.blade.php
@@ -0,0 +1,21 @@
+
+@extends('layout')
+@section('titulo', 'exercicio 1')
+@section('conteudo')
+
+        <h1>exercicio 1 </h1>
+        <form method="post" action="/exer1resp">
+            <div class="mb-3">
+                <label for="valor1" class="form-label">informe o primeiro valor</label>
+                <input type="number" id="valor1" name="valor1" class="form-control" required="">
+            </div>
+            <div class="mb-3">
+                <label for="valor2" class="form-label">informe o segundo valor para somar</label>
+                <input type="number" id="valor2" name="valor2" class="form-control" required="">
+            </div>
+            <button type="submit" class="btn btn-primary">Enviar</button>
+        </form>
+        @isset($soma)
+            <p> Soma: {{ $soma}} </p>
+        @endisset
+@endsection
